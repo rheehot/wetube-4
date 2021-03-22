6a372e79c9a82541bcc840cafdd23894df068152
nahyunbak <61341571+nahyunbak@users.noreply.github.com>
2021-03-23 02:54:01 +0900

    before pug

---
+++ controllers/videoController.js
@@ -0,0 +1,9 @@
+//컨트롤러 추가 
+//서식: export const 상수 = 변수 => 결과  
+export const home = (req, res) => res.send("Home");
+export const search = (req, res) => res.send("Search");
+export const videos = (req, res) => res.send("Videos");
+export const upload = (req, res) => res.send("Upload");
+export const videoDetail = (req, res) => res.send("Video Detail");
+export const editVideo = (req, res) => res.send("Edit Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
